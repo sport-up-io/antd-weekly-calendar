bf51a25da3014cbd6d251e57514da55b253fef33
William Donnette <willdonn17@gmail.com>
2026-01-08 23:00:00 +0100

    fix: update npm publish command to include npm prune and bump version to 2.1.5

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,45 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    plugins: [react(), dts({
-        tsconfigPath: './tsconfig.build.json',
-    }),],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),  // This is correct
-            name: 'AntdWeeklyCalendar',
-            fileName: (format) => `antd-weekly-calendar.${format}.js`,
-            formats: ['es', 'umd'],  // Library output formats
-        },
-        rollupOptions: {
-            external: ['react', 'react-dom', 'antd', '@ant-design/cssinjs'],  // Peer dependencies
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM',
-                        antd: 'antd',
-    '@ant-design/cssinjs': 'cssinjs'
-                },
-            },
-        },
-
-        emptyOutDir: false,  // Prevent Vite from clearing the output directory
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: './tsconfig.build.json',
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'), // This is correct
+      name: 'AntdWeeklyCalendar',
+      fileName: (format) => `antd-weekly-calendar.${format}.js`,
+      formats: ['es', 'umd'], // Library output formats
     },
+    rollupOptions: {
+      external: (id) =>
+        id === 'react' ||
+        id === 'react-dom' ||
+        id.startsWith('react/') ||
+        id.startsWith('react-dom/') ||
+        id === 'antd' ||
+        id.startsWith('antd/') ||
+        id === '@ant-design/cssinjs',
+      output: {
+        globals: {
+          react: 'React',
+          'react/jsx-runtime': 'ReactJSXRuntime',
+          'react/jsx-dev-runtime': 'ReactJSXDevRuntime',
+          'react-dom': 'ReactDOM',
+          'react-dom/client': 'ReactDOMClient',
+          antd: 'antd',
+          '@ant-design/cssinjs': 'cssinjs',
+          'antd/es/date-picker/generatePicker': 'antdDatePickerGeneratePicker',
+        },
+      },
+    },
+
+    emptyOutDir: false, // Prevent Vite from clearing the output directory
+  },
 });
